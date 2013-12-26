ca0579c6ff0a48cfd67d8dca363b8869321e4fe8
alexgus <alex.guyon78@gmail.com>
2013-12-26 16:16:21 +0000

    - Added Behaviour class for entity

---
+++ src/fr/utbm/lo43/jvivarium/core/Behaviour.java
@@ -0,0 +1,27 @@
+package fr.utbm.lo43.jvivarium.core;
+
+/**
+ * Behaviour of an entity.
+ * The entity will think in function of
+ * all the attributes listed here.
+ * @author Alexandre Guyon
+ */
+public class Behaviour
+{
+	private int shy;
+	private int curious;
+	// Etc
+	
+	/**
+	 * 
+	 */
+	public Behaviour()
+	{
+		
+	}
+	
+	public void move()
+	{
+		
+	}
+}
