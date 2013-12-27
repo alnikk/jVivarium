644dc3d21043826376e8d9efd2e6360cad5af018
alexgus <alex.guyon78@gmail.com>
2013-12-27 19:38:21 +0100

    - Fixed inheritance bug of Obj class

--- src/fr/utbm/lo43/jvivarium/core/Obj.java
+++ src/fr/utbm/lo43/jvivarium/core/Obj.java
@@ -4,7 +4,7 @@ package fr.utbm.lo43.jvivarium.core;
  * Objects for help (or not ^^) entity
  * @author Alexandre Guyon
  */
-public final class Obj
+public final class Obj extends Element
 {
 	/**
 	 * Type of this object
@@ -16,8 +16,27 @@ public final class Obj
 	 * Define the type of this object.
 	 * @param t The type of object you want to create
 	 */
-	public Obj(ObjectType t)
+	public Obj(BoundingBox b, ObjectType t)
 	{
-		this.type = t;
+		super(b);
+		this.setType(t);
+	}
+
+	/**
+	 * Get the type of object
+	 * @return the type of the object
+	 */
+	public ObjectType getType()
+	{
+		return type;
+	}
+
+	/**
+	 * Ste the type of this object
+	 * @param type the type of object to set
+	 */
+	public void setType(ObjectType type)
+	{
+		this.type = type;
 	}
 }
