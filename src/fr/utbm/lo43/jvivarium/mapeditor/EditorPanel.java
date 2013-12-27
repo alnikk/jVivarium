21d6ff1521824bf25d3ba92ebb2fa34b66741844
alexgus <alex.guyon78@gmail.com>
2013-12-27 22:07:04 +0100

    - Added view of Entity and Objects

--- src/fr/utbm/lo43/jvivarium/mapeditor/EditorPanel.java
+++ src/fr/utbm/lo43/jvivarium/mapeditor/EditorPanel.java
@@ -10,18 +10,26 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import fr.utbm.lo43.jvivarium.core.BoundingBox;
+import fr.utbm.lo43.jvivarium.core.Bowser;
 import fr.utbm.lo43.jvivarium.core.Chunk;
 import fr.utbm.lo43.jvivarium.core.Coordinates;
 import fr.utbm.lo43.jvivarium.core.Element;
 import fr.utbm.lo43.jvivarium.core.Entity;
 import fr.utbm.lo43.jvivarium.core.Map;
+import fr.utbm.lo43.jvivarium.core.Mario;
 import fr.utbm.lo43.jvivarium.core.Obj;
+import fr.utbm.lo43.jvivarium.core.ObjectType;
+import fr.utbm.lo43.jvivarium.core.Peach;
 import fr.utbm.lo43.jvivarium.core.XMLLoader;
 
 /**
@@ -114,11 +122,101 @@ public class EditorPanel extends JPanel implements Runnable, MouseListener, Mous
 		super.paint(g);
 		
 		// Draw all chunks
-		List<Chunk> l = this.map.getChunks();
-		for (Iterator<Chunk> it = l.iterator(); it.hasNext();)
+		List<Chunk> lc = this.map.getChunks();
+		for(Iterator<Chunk> it = lc.iterator(); it.hasNext();)
 		{
 			Chunk c = it.next();
 			c.paint(g);
+		}
+		
+		// Draw all entity
+		List<Entity> le = this.map.getEntitys();
+		for(Iterator<Entity> it = le.iterator(); it.hasNext();)
+		{
+			Entity e = it.next();
+			
+			BufferedImage img = null;
+			if(e instanceof Mario)
+			{
+				try
+				{
+					img = ImageIO.read(new File(Mario.IMG));
+				}
+				catch (IOException e1)
+				{
+					e1.printStackTrace();
+				}
+			}
+			if(e instanceof Peach)
+			{
+				try
+				{
+					img = ImageIO.read(new File(Peach.IMG));
+				}
+				catch (IOException e1)
+				{
+					e1.printStackTrace();
+				}
+			}
+			if(e instanceof Bowser)
+			{
+				try
+				{
+					img = ImageIO.read(new File(Bowser.IMG));
+				}
+				catch (IOException e1)
+				{
+					e1.printStackTrace();
+				}
+			}
+			if(img != null)
+			{
+				g.drawImage(img, 
+						e.getArea().getPosition().getX(), 
+						e.getArea().getPosition().getY(), 
+						e.getArea().getSize().getX(), 
+						e.getArea().getSize().getY(), null);
+			}
+		}
+		
+		// Draw all objects
+		List<Obj> lo = this.map.getObjects();
+		for(Iterator<Obj> it = lo.iterator(); it.hasNext();)
+		{
+			Obj o = it.next();
+			
+			BufferedImage img = null;
+			
+			if(o.getType() == ObjectType.MUSHROOM)
+			{
+				try
+				{
+					img = ImageIO.read(new File(Obj.MUSHROOM));
+				}
+				catch (IOException e1)
+				{
+					e1.printStackTrace();
+				}
+			}
+			if(o.getType() == ObjectType.STAR)
+			{
+				try
+				{
+					img = ImageIO.read(new File(Obj.STAR));
+				}
+				catch (IOException e1)
+				{
+					e1.printStackTrace();
+				}
+			}
+			if(img != null)
+			{
+				g.drawImage(img, 
+						o.getArea().getPosition().getX(), 
+						o.getArea().getPosition().getY(), 
+						o.getArea().getSize().getX(), 
+						o.getArea().getSize().getY(), null);
+			}
 		}
 	}
 	
