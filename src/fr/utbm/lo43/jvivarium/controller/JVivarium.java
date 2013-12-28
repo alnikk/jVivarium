bda206003244c037fd7d83b7b17f75c2a0064925
alexgus <alex.guyon78@gmail.com>
2013-12-28 11:44:48 +0100

    - Renamed the main for conflict reason - Added the contents of the main into the main class controller

--- src/fr/utbm/lo43/jvivarium/controller/JVivarium.java
+++ src/fr/utbm/lo43/jvivarium/controller/JVivarium.java
@@ -1,5 +1,18 @@
 package fr.utbm.lo43.jvivarium.controller;
 
+import java.util.Iterator;
+import java.util.List;
+
+import fr.utbm.lo43.jvivarium.core.BoundingBox;
+import fr.utbm.lo43.jvivarium.core.Bowser;
+import fr.utbm.lo43.jvivarium.core.Coordinates;
+import fr.utbm.lo43.jvivarium.core.Entity;
+import fr.utbm.lo43.jvivarium.core.Map;
+import fr.utbm.lo43.jvivarium.core.Mario;
+import fr.utbm.lo43.jvivarium.core.NegativeSizeException;
+import fr.utbm.lo43.jvivarium.core.Peach;
+import fr.utbm.lo43.jvivarium.view.MainFrame;
+
 /**
  * Main class of the project.
  * Used to controlle entity and verify them
@@ -7,10 +20,56 @@ package fr.utbm.lo43.jvivarium.controller;
  */
 public final class JVivarium
 {
-
+	/**
+	 * Main frame of the game
+	 */
+	MainFrame mFrame = new MainFrame();
+	
+	/**
+	 * Instance of the map
+	 */
+	Map map = Map.getMap();
+	
+	
 	public JVivarium()
 	{
+		XMLLoader xml = new XMLLoader();
+		xml.startParse();
 		
+		// Create entity
+		try
+		{
+			map.add(new Mario(new BoundingBox(new Coordinates(60, 200), new Coordinates(20, 20))));
+			map.add(new Peach(new BoundingBox(new Coordinates(20, 300), new Coordinates(20, 20))));
+			map.add(new Bowser(new BoundingBox(new Coordinates(40, 0), new Coordinates(20, 20))));
+		}
+		catch (NegativeSizeException e)
+		{
+			e.printStackTrace();
+		}
 	}
-
+	
+	public void start()
+	{
+		List<Entity> l = this.map.getEntitys();
+		Entity e;
+		
+		while(true)
+		{
+			for(Iterator<Entity> it = l.iterator(); it.hasNext();)
+			{
+				e = it.next();
+				e.life();
+			}
+			
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch (InterruptedException e1)
+			{
+				e1.printStackTrace();
+			}
+		}
+	}
 }
---
+++ src/fr/utbm/lo43/jvivarium/testunit/JVivariumMain.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package fr.utbm.lo43.jvivarium.testunit;
+
+import fr.utbm.lo43.jvivarium.controller.JVivarium;
+
+/**
+ * Main of the project
+ * @author Alexandre Guyon
+ */
+public class JVivariumMain
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		JVivarium j = new JVivarium();
+		j.start();
+	}
+
+}
