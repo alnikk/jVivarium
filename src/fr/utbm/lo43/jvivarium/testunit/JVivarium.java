6b93d1117cfa5b4b02355f8afbf73749b328e4a2
alexgus <alex.guyon78@gmail.com>
2013-12-28 11:33:15 +0100

    - Created main controller

--- src/fr/utbm/lo43/jvivarium/testunit/JVivarium.java
+++ src/fr/utbm/lo43/jvivarium/testunit/JVivarium.java
@@ -3,7 +3,20 @@
  */
 package fr.utbm.lo43.jvivarium.testunit;
 
+import java.util.Iterator;
+import java.util.List;
+
+import fr.utbm.lo43.jvivarium.core.BoundingBox;
+import fr.utbm.lo43.jvivarium.core.Bowser;
+import fr.utbm.lo43.jvivarium.core.Coordinates;
+import fr.utbm.lo43.jvivarium.core.Entity;
+import fr.utbm.lo43.jvivarium.core.Map;
+import fr.utbm.lo43.jvivarium.core.Mario;
+import fr.utbm.lo43.jvivarium.core.NegativeSizeException;
+import fr.utbm.lo43.jvivarium.core.Peach;
+import fr.utbm.lo43.jvivarium.core.XMLLoader;
 import fr.utbm.lo43.jvivarium.mapeditor.MapEditor;
+import fr.utbm.lo43.jvivarium.view.MainFrame;
 
 /**
  * Main class of the package
@@ -17,8 +30,45 @@ public class JVivarium
 	 */
 	public static void main(String[] args)
 	{
-		MapEditor m = new MapEditor();
-		m.start();
+		XMLLoader xml = new XMLLoader();
+		xml.startParse();
+		
+		MainFrame m = new MainFrame();
+		
+		
+		// Create entity
+		Map map = Map.getMap();
+		try
+		{
+			map.add(new Mario(new BoundingBox(new Coordinates(60, 200), new Coordinates(20, 20))));
+			map.add(new Peach(new BoundingBox(new Coordinates(20, 300), new Coordinates(20, 20))));
+			map.add(new Bowser(new BoundingBox(new Coordinates(40, 0), new Coordinates(20, 20))));
+		}
+		catch (NegativeSizeException e)
+		{
+			e.printStackTrace();
+		}
+		
+		List<Entity> l = map.getEntitys();
+		Entity e;
+		
+		while(true)
+		{
+			for(Iterator<Entity> it = l.iterator(); it.hasNext();)
+			{
+				e = it.next();
+				e.life();
+			}
+			
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch (InterruptedException e1)
+			{
+				e1.printStackTrace();
+			}
+		}
 	}
 
 }
