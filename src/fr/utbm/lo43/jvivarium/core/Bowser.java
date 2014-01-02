f5991748bdc6b1e8446faa0879e8d602949b2fba
alexgus <alex.guyon78@gmail.com>
2014-01-02 08:25:07 +0100

    - Added first behaviour in Bowser

--- src/fr/utbm/lo43/jvivarium/core/Bowser.java
+++ src/fr/utbm/lo43/jvivarium/core/Bowser.java
@@ -1,20 +1,43 @@
 package fr.utbm.lo43.jvivarium.core;
 
+import java.util.Iterator;
+
 public final class Bowser extends Entity
 {
+	//********************** Constants ********************
 	/**
 	 * The image file for Bowser
 	 */
 	public final static String IMG = "./res/entity/bowser1.png";
 	
 	/**
+	 * The max Vision of a Bowser entity
+	 */
+	private final static int VISION = 200;
+	
+	/**
+	 * The attack point of a Bowser Entity
+	 */
+	private final static int ATTACK = 15;
+	
+	/**
+	 * The move of a Bowser entity
+	 */
+	private final static int MOVE = 3;
+	
+	//************************* Attributes ****************
+	/**
 	 * If browser kidnap Peach
 	 */
 	private Peach kidnaped = null;
 	
+	//**************************** Constructor ****************
 	public Bowser(BoundingBox area)
 	{
 		super(area);
+		this.attPoints = ATTACK;
+		this.vision = VISION;
+		this.move = MOVE;
 	}
 
 	public void finalize()
@@ -22,69 +45,76 @@ public final class Bowser extends Entity
 		if(kidnaped != null)
 			Map.getMap().add(kidnaped);
 	}
+	
+	//****************************** Methods ********************
+	
 	@Override
 	public void life()
 	{
-		this.move();
+		// Allow to act only one time
+		boolean act = false;
+		Entity e;
 		
-		//if Bowser and Peach are at the same place, he will kidnap her
-		if(Map.getMap().getEntityAt(this.getArea().getPosition()) != null && Map.getMap().getEntityAt(this.getArea().getPosition()) instanceof Peach)
-			this.kidnap();
+		if(kidnaped == null)
+		{
+			for(Iterator<Entity> itp = Map.getMap().searchEntity(Peach.class).iterator(); itp.hasNext();)
+			{
+				e = itp.next();
+				
+				if(this.overlapping(e) != null)
+				{
+					this.kidnap((Peach) e);
+					act = true;
+					break;
+				}
+			}
+		}
+		
+		if(!act)
+		{
+			for(Iterator<Entity> itp = Map.getMap().searchEntity(Mario.class).iterator(); itp.hasNext();)
+			{
+				e = itp.next();
+				
+				if(this.overlapping(e) != null)
+				{
+					this.attack(e);
+					act = true;
+					break;
+				}
+			}
+		}
+		
+		if(!act)
+			this.move();
 	}
 	
 	private void move()
 	{
-		Coordinates max = Map.getMap().getMaxMap();
-		BoundingBox b = this.getArea();
-		int x,y;
+		Element e = Map.getMap().searchNearest(this, Peach.class);
 		
-		// Check valid coordinates
-		do
-		{
-			// Choice of coordinates
-			x = (int) Math.round(Math.random()*20);
-			if(Math.random() < 0.5)
-				x = -x;
-			
-			y = (int) Math.round(Math.random()*20);
-			if(Math.random() < 0.5)
-				y = -y;
-			
-			// test
-			try
-			{
-				b = this.getArea().translate(new Coordinates(x, y));
-			}
-			catch (NegativeSizeException e)
-			{
-				e.printStackTrace();
-			}
-		}while((b.getPosition().getX() < 0 
-				|| b.getPosition().getX() + b.getSize().getX() > max.getX())
-				|| (b.getPosition().getY() < 0
-				|| b.getPosition().getY() + b.getSize().getY() > max.getY()));
-		
-		// Set new position
-		this.setArea(b);	
+		if(e != null)
+			this.goNearTo(e,this.move);
 	}
 	
-	private void attack()
+	private void attack(Entity e)
 	{
+		// Bowser lose life points, he lose more life points than Mario
+		e.setLifePoints(getLifePoints() - this.attPoints);
 		
+		// TODO In the main loop I think... (turn by turn effect will be better, and so order in the list no matter)
+		if(e.getLifePoints() == 0)
+			Map.getMap().remove(e);
 	}
 	
-	private void kidnap()
+	private void kidnap(Peach p)
 	{
-		// if Bowser has not already kidnaped Peach
 		if(kidnaped == null)
 		{
-			// this Peach has been kidnaped and stocked in memory
-			kidnaped = (Peach) Map.getMap().getEntityAt(this.getArea().getPosition());
-		
+			// if Bowser has not already kidnaped Peach
+			kidnaped = p;
 			// we remove her of the map
-			Map.getMap().remove(Map.getMap().getEntityAt(this.getArea().getPosition()));
+			Map.getMap().remove(p);
 		}
 	}
-	
-
 }
