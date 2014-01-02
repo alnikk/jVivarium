bf2aad86b49ddb9e8ca627b754718b9161ff3a70
alexgus <alex.guyon78@gmail.com>
2014-01-02 08:10:53 +0100

    - Added first behaviour

--- src/fr/utbm/lo43/jvivarium/core/Mario.java
+++ src/fr/utbm/lo43/jvivarium/core/Mario.java
@@ -1,101 +1,142 @@
 package fr.utbm.lo43.jvivarium.core;
 
-import java.util.Random;
+import java.util.Iterator;
 
 public final class Mario extends Entity
 {
+	//******************************** Constant ***************************
+	
 	/**
 	 * The image file for Mario
 	 */
 	public final static String IMG = "./res/entity/mario1.png";
 	
+	/**
+	 * The max Vision of a Mario entity
+	 */
+	private final static int VISION = 200;
+	
+	/**
+	 * The attack point of a Mario Entity
+	 */
+	private final static int ATTACK = 20;
+	
+	/**
+	 * The move of a Mario entity
+	 */
+	private final static int MOVE = 3;
+	
+	//********************************* Attributes ************************
+	
+	
+	
+	//********************************* Constructor ************************
+	
 	public Mario(BoundingBox area)
 	{
 		super(area);
+		this.attPoints = ATTACK;
+		this.vision = VISION;
+		this.move = MOVE;
 	}
 
+	//********************************* Methods *****************************
+	
 	@Override
 	public void life()
 	{
-		this.move();
+		// Allow the Mario to only executes one action
+		boolean act = false;
+		Entity b = null;
+		Obj o = null;
+		
+		// If Mario and Bowser touch them-self, attack
+		for(Iterator<Entity> itb = Map.getMap().searchEntity(Bowser.class).iterator(); itb.hasNext();)
+		{
+			b = itb.next();
+			
+			// If they are overlapping
+			if(this.overlapping(b) != null)
+			{
+				this.attack((Bowser) b);
+				act = true;
+				break;
+			}
+		}
 		
 		// if Mario is on an Obj, he will eat it
-		if(Map.getMap().getObjAt(this.getArea().getPosition()) != null)
-			this.eat();
+		if(!act)
+		{
+			for(Iterator<Obj> ito = Map.getMap().getObjects().iterator(); ito.hasNext();)
+			{
+				o = ito.next();
+				
+				if(this.overlapping(o) != null)
+				{
+					this.eat(o);
+					act = true;
+					break;
+				}
+			}
+		}
 		
-		//if Mario and Bowser are at the same they will attack themself
-		if(Map.getMap().getEntityAt(this.getArea().getPosition()) != null && Map.getMap().getEntityAt(this.getArea().getPosition()) instanceof Bowser)
-			this.attack();
+		if(!act)
+			this.move();
+	}
+	
+	/**
+	 * Mario will attack Bowser
+	 */
+	private void attack(Bowser b)
+	{
+		// Bowser lose life points, he lose more life points than Mario
+		b.setLifePoints(getLifePoints() - this.attPoints);
+		
+		// TODO In the main loop I think... (turn by turn effect will be better, and so order in the list no matter)
+		if(b.getLifePoints() == 0)
+			Map.getMap().remove(b);
+	}
+	
+	/**
+	 * Mario will eat something on the map
+	 */
+	private void eat(Obj o)
+	{
+		// if the object is a MUSHROOM, we regenerate the life points of Mario
+		switch(Map.getMap().getObjAt(this.getArea().getPosition()).getType())
+		{
+			case MUSHROOM:
+				this.setLifePoints(this.MAX_LIFE);
+				break;
+			case STAR:
+				System.out.println("Mario eats a star");
+				break;
+		}
+		
+		//Mario eat this object, so we can remove it from the map
+		Map.getMap().remove(Map.getMap().getObjAt(this.getArea().getPosition()));
 	}
 	
 	/**
 	 * This Mario object will move on the map
 	 */
 	private void move()
-	{
-		Coordinates max = Map.getMap().getMaxMap();
-		BoundingBox b = this.getArea();
-		int x,y;
+	{		
+		Entity p = Map.getMap().searchNearest(this, Peach.class);
+		Entity b = Map.getMap().searchNearest(this, Bowser.class);
+		Element e;
 		
-		// Check valid coordinates
-		do
+		if(p == null)
+			this.goNearTo(b, this.move);
+		else if(b == null)
+			this.goNearTo(p, this.move);
+		else
 		{
-			// Choice of coordinates
-			x = (int) Math.round(Math.random()*20);
-			if(Math.random() < 0.5)
-				x = -x;
-			
-			y = (int) Math.round(Math.random()*20);
-			if(Math.random() < 0.5)
-				y = -y;
-			
-			// test
-			try
-			{
-				b = this.getArea().translate(new Coordinates(x, y));
-			}
-			catch (NegativeSizeException e)
-			{
-				e.printStackTrace();
-			}
-		}while((b.getPosition().getX() < 0 
-				|| b.getPosition().getX() + b.getSize().getX() > max.getX())
-				|| (b.getPosition().getY() < 0
-				|| b.getPosition().getY() + b.getSize().getY() > max.getY()));
-		
-		// Set new position
-		this.setArea(b);	
+			e = Map.compNear(this, b, p);
+			if(e != null)
+				this.goNearTo(e, this.move);
+			else // Bowser for combativity
+				this.goNearTo(b, this.move);
+		}
 	}
-	
-	/**
-	 * Mario will eat something on the map
-	 */
-	private void eat()
-	{
-		// if the object is a MUSHROOM, we regenerate the life points of Mario
-		if(Map.getMap().getObjAt(this.getArea().getPosition()).getType() == ObjectType.MUSHROOM)
-			this.setLifePoints(this.MAX_LIFE);
-		
-		//Mario eat this object, so we can remove it from the map
-		Map.getMap().remove(Map.getMap().getObjAt(this.getArea().getPosition()));
-	}
-	
-
-	
-	/**
-	 * Mario will attack Bowser
-	 */
-	private void attack()
-	{
-		// Bowser lose life points, he lose more life points than Mario
-		Bowser b = (Bowser) Map.getMap().getEntityAt(this.getArea().getPosition());
-		b.setLifePoints(getLifePoints() - getLifePoints());
-		
-		// And so do Mario
-		this.setLifePoints(getLifePoints() - this.attPoints);
-		
-		if(b.getLifePoints() == 0)
-			Map.getMap().remove(b);
-	}
-
 }
